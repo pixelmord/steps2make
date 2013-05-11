b607e3a6efab8ff4fc2e6b212b769c186640251e
Andreas Sahle <as@wunderkraut.com>
2013-05-11 20:37:37 +0200

    add remove button for ingredients

--- app/scripts/controllers/createProject.js
+++ app/scripts/controllers/createProject.js
@@ -4,18 +4,7 @@ angular.module('steps2makeApp')
   .controller('CreateProjectCtrl', function ($scope, $location, Project) {
     $scope.project = {};
     $scope.project.stepsCount = 0;
-    $scope.project.ingredients = [
-      {
-        quantity: 12,
-        measurement: 'pcs.',
-        title: 'Testingredient'
-      },
-      {
-        quantity: 102,
-        measurement: 'small',
-        title: 'Nails'
-      }
-    ];
+    $scope.project.ingredients = [{}];
     $scope.save = function() {
       Project.save($scope.project, function(project) {
         $location.path('/edit/' + project._id.$oid);
@@ -25,7 +14,7 @@ angular.module('steps2makeApp')
   .directive('stmIngredient', function() {
     return {
       restrict: 'E',
-      template: '<fieldset class="form-inline">\n  <div class="input-prepend">\n    <span class="add-on">#</span>\n    <input class="input-mini" type="number" ng-model="ingredient.quantity">\n  </div>\n  <input class="input-small" type="text" name="" placeholder="Measurement" ng-model="ingredient.measurement"/>\n  <input class="input-large" type="text" name="" placeholder="Ingredient" ng-model="ingredient.title"/>\n</fieldset>'
+      template: '<fieldset class="form-inline">\n  <div class="input-prepend">\n    <span class="add-on">#</span>\n    <input class="input-mini" type="number" min="0" ng-model="ingredient.quantity">\n  </div>\n  <input class="input-small" type="text" name="" placeholder="Measurement" ng-model="ingredient.measurement"/>\n  <input class="input-large" type="text" name="" placeholder="Ingredient" ng-model="ingredient.title"/>\n  <button class="btn btn-danger" ng-click="removeIngredient($index)">X</button>\n</fieldset>'
     };
   })
   .directive('stmIngredients', function() {
@@ -36,6 +25,9 @@ angular.module('steps2makeApp')
       link: function(scope) {
         scope.addIngredient = function () {
           scope.project.ingredients.push({});
+        };
+        scope.removeIngredient = function (index) {
+          scope.project.ingredients.splice(index, 1);
         };
       }
     };
